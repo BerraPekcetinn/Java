745fd36b5f5baf94b3870daa0d8f95a64f9d40bc
BerraPekcetinn <169042017+BerraPekcetinn@users.noreply.github.com>
2024-06-11 15:53:50 +0100

    first commit

---
+++ src/day48_maps/C01_OgrenciNumarasinaGoreListeYazdir.java
@@ -0,0 +1,17 @@
+package day48_maps;
+
+import day47_maps.OgrenciMapClass;
+
+public class C01_OgrenciNumarasinaGoreListeYazdir extends OgrenciMapClass {
+
+
+    public static void main(String[] args) {
+
+       // - Ogrenci numarasi olarak verilen min ve max (sinirlar dahil) numaralar
+        //arasindaki tum ogrencilerin numara, isim, soyisim, sinif ve subelerini yazdirin
+
+
+
+    }
+
+}
